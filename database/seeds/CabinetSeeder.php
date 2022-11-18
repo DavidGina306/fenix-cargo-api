824411bdc25a1b5e75e9e7c8e4a73bb5b9966f10
David Felipe Santos Oliveira <davidfelipe_306@hotmail.com>
2022-11-18 20:02:56 -0300

    add

--- database/seeds/CabinetSeeder.php
+++ database/seeds/CabinetSeeder.php
@@ -17,12 +17,22 @@ class CabinetSeeder extends Seeder
      */
     public function run()
     {
+        $address = Address::query()->firstOrCreate([
+            'address_line_1' => "rua 1",
+            'address_line_2' => "adrianopolis",
+            'address_line_3' => "",
+            'town' => "Manaus",
+            'country' => "Brasil",
+            'state' => "AM",
+            'postcode' => "69000000"
+        ]);
         for ($j = 0; $j < rand(5, 25); $j++) {
+
             $cabinet = Cabinet::query()->firstOrCreate(
                 [
                     'order' => substr(str_shuffle(time() . mt_rand(0, 999) . md5(time() . mt_rand(0, 999))), 0, 16),
                     'customer_id' => Customer::first()->id,
-                    'address_id' => Address::first()->id,
+                    'address_id' => $address->id,
                     'status' => 'R',
                     'doc_value' => MoneyToDecimal::moneyToDecimal(100),
                     'storage_locale' => "Armazém Abandonado",

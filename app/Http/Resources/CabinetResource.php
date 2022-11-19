99fa2a01116e22093a51c801241f2a63cf3d90ae
David Felipe Santos Oliveira <davidfelipe_306@hotmail.com>
2022-11-18 23:55:24 -0300

    add values

--- app/Http/Resources/CabinetResource.php
+++ app/Http/Resources/CabinetResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CabinetResource extends JsonResource
@@ -16,7 +17,7 @@ class CabinetResource extends JsonResource
     {
         return [
             "order" => $this->order,
-            "entry_date" => $this->entry_date,
+            "entry_date" =>  Carbon::parse($this->entry_date)->format('d M Y') ,
             "id" => $this->id,
             "out_date" => $this->out_date,
             "status" => $this->status,
